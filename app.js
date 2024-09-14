0bee64a32a43e122ebb5996860ed1fda1a0cff10
MuhammadAli206 <154820260+MuhammadAli206@users.noreply.github.com>
2024-09-14 08:29:21 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+let btn = document.getElementById('btn')
+
+
+function MyFunction () {
+    let element = document.body;
+    element.classList.toggle('red')
+}
+
+function MyFunctions () {
+    btn.classList.toggle('blue')
+}
+
+
